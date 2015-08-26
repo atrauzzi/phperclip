b25347cdf2dbf32294954cffe56b8a1c2c85997c
Alexander Trauzzi <atrauzzi@gmail.com>
2015-08-26 17:42:39 -0500

    Don't generate derivatives when obtaining a resource.

--- src/Service.php
+++ src/Service.php
@@ -115,14 +115,7 @@
 
 			$disk = $this->getDisk()->getDriver();
 			$path = $this->filePath($fileMeta, $options);
-
-			// If it isn't the original and it hasn't been generated yet.
-			if(count($options))
-				$resource = $this->ensureDerivative($fileMeta, $options);
-			else
-				$resource = $disk->readStream($path);
-
-			return $resource;
+			return $disk->readStream($path);
 
 		}
 
