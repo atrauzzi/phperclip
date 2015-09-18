3b3458a8cdfda4a70bcbfb6a96746f6a1447b17d
Alexander Trauzzi <atrauzzi@gmail.com>
2015-09-18 10:54:56 -0500

    Let's make file names opt-in.

--- database/migrations/2015_03_26_000001_create_file_meta_table.php
+++ database/migrations/2015_03_26_000001_create_file_meta_table.php
@@ -17,7 +17,7 @@ class CreateFileMetaTable extends Migration {
 
 			$table->increments('id');
 
-			$table->string('name')->unique();
+			$table->string('name')->unique()->nullable();
 			$table->string('disk');
 			$table->string('mime_type');
 
